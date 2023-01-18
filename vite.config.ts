53b53425bc2548525bd72d13b9982994f916d783
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-01-18 21:51:51 +0100

    :wrench: project name in vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: '/SVG-animation-acronym/',
+  define: {
+    'process.env': {},
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/scss/_variables.scss";',
+      },
+    },
+  },
+});

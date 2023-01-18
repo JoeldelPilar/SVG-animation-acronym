1280c34a344ef6a7290893479da3998f96bbf26d
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-01-18 20:26:54 +0100

    :wrench: deploy files + build

---
+++ src/components/AcronymAnimation.vue
@@ -0,0 +1,30 @@
+<!-- ********************************** HTML ********************************************* -->
+<template>
+	<h1>Hello World!</h1>
+	<div class="img-container">
+		<img
+			src="/jaydepi-free.svg"
+			alt="the acronym jay de pi, with pink small squares under that forms a straigt line."
+			width="300"
+			height="200"
+		/>
+	</div>
+</template>
+
+<!-- ********************************** script ********************************************* -->
+
+<script setup lang="ts"></script>
+
+<!-- ********************************** Styles ********************************************* -->
+
+<style scoped>
+	.img-container {
+		position: relative;
+		width: 80vw;
+		margin-inline: auto;
+	}
+
+	img {
+		height: auto;
+	}
+</style>

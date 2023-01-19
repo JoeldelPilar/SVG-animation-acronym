664cfc1f4263870ba17a88120421497d6b1300a9
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-01-19 10:25:42 +0100

    :lipstick: gsap animation

--- src/components/AcronymAnimation.vue
+++ src/components/AcronymAnimation.vue
@@ -1,20 +1,37 @@
 <!-- ********************************** HTML ********************************************* -->
 <template>
 	<div class="img-container">
-		<img
-			src="/jaydepi-free.svg"
-			alt="the acronym jay de pi, with pink small squares under that forms a straigt line."
-			width="300"
-			height="200"
-		/>
+		<SvgAcronym />
 	</div>
 </template>
 
 <!-- ********************************** script ********************************************* -->
 
 <script lang="ts">
+	import { gsap } from "gsap";
+	import SvgAcronym from "./SvgAcronym.vue";
+
 	export default {
 		name: "AcronymAnimation",
+		components: {
+			SvgAcronym,
+		},
+		data() {
+			return {};
+		},
+		mounted() {
+			this.$nextTick().then(() => {
+				for (let i = 1; i <= 40; i++) {
+					gsap.to(`#r${i}`, {
+						duration: 0.5,
+						opacity: 1,
+						delay: i * 0.2,
+						stagger: 0.1,
+					});
+				}
+			});
+			gsap.to("#j, #d, #p", { rotate: 15, delay: 8, duration: 1 });
+		},
 	};
 </script>
 
@@ -24,9 +41,7 @@
 	.img-container {
 		position: relative;
 		margin-inline: auto;
-	}
-
-	img {
+		background-color: transparent;
 		width: 70vw;
 		height: auto;
 	}
